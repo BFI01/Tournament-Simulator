c8b18488352b3d208ad57df819dbd83da63a9322
BFI01 <bfi01messages@gmail.com>
2022-05-01 15:40:17 +0100

    Overloaded playMatch to account for Byes

--- src/tournament/Match.java
+++ src/tournament/Match.java
@@ -9,6 +9,10 @@ public class Match extends Game {
     // But makes it so you can't track how many wins each player has after the tournament ends
     private final HashMap<String, Integer> gamesDict = new HashMap<>();
 
+    private void updatePlayerWins(Player player) {
+        player.setWins(player.getWins() + 1);
+    }
+
     public Player playMatch(Player matchPlayer1, Player matchPlayer2) {
         gamesDict.put("player1", 0);
         gamesDict.put("player2", 0);
@@ -24,6 +28,17 @@ public class Match extends Game {
         while (gamesDict.get("player1") < winsNeeded &&
                gamesDict.get("player2") < winsNeeded);
 
-        return (gamesDict.get("player1") > gamesDict.get("player2")) ? matchPlayer1 : matchPlayer2;
+        if (gamesDict.get("player1") > gamesDict.get("player2")) {
+            updatePlayerWins(matchPlayer1);
+            return matchPlayer1;
+        } else {
+            updatePlayerWins(matchPlayer2);
+            return matchPlayer2;
+        }
+    }
+
+    public Player playMatch(Player matchPlayer, boolean bye) {
+        // When a match contains a Bye, the player immediately moves on to the next round
+        return matchPlayer;
     }
 }
