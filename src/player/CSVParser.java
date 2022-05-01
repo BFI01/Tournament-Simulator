4065bb8d0ae44059a3aa06a9765d8cef0df1d898
BFI01 <bfi01messages@gmail.com>
2022-05-01 15:03:47 +0100

    Changed functionality from JSON to CSV using [openCSV](http://opencsv.sourceforge.net/)

--- src/player/CSVParser.java
+++ src/player/CSVParser.java
@@ -1,16 +1,15 @@
+package player;
+
 import com.opencsv.CSVReader;
 import com.opencsv.exceptions.CsvException;
 
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.Arrays;
 import java.util.List;
 
 public class CSVParser {
     public List<String[]> readFromFile(String filePath) throws IOException, CsvException {
         CSVReader reader = new CSVReader(new FileReader(filePath));
-        List<String[]> data = reader.readAll();
-        System.out.println(Arrays.toString(data.toArray()));
-        return data;
+        return reader.readAll();
     }
 }
