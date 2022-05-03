f334105d037c69239e7d341b1337aa9dce75936a
BFI01 <bfi01messages@gmail.com>
2022-05-03 17:11:41 +0100

    Implemented Tournament object and CSV functionality

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,17 @@
+import com.opencsv.exceptions.CsvException;
+import player.CSVParser;
 import player.Player;
 import tournament.Match;
+import tournament.Tournament;
 
-import java.io.FileNotFoundException;
-import java.util.ArrayList;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) throws IOException, CsvException {
         System.out.println("Table Tennis Tournament Simulator");
 
         // Get user input for number of players in the tournament
@@ -15,10 +19,11 @@ public class Main {
         int playerCount = -1;
         do {
             System.out.println("Enter the number of players in the tournament:");
+            System.out.print("> ");
             try {
                 playerCount = scanner.nextInt();
-                if (playerCount < 2 || playerCount > 1000) {
-                    System.out.println("Input must be between 2 and 1000! Please try again...");
+                if (playerCount < 2) {
+                    System.out.println("You need at least two players! Please try again...");
                 }
             }
             catch (java.util.InputMismatchException e) {
@@ -26,26 +31,36 @@ public class Main {
                 scanner.next();
             }
         }
-        while (playerCount < 2 || playerCount > 1000);
+        while (playerCount < 2);
 
-        // Read player information from JSON file and create array of players
-        JSONParser jsonParser = new JSONParser();
-        ArrayList<Player> players = new ArrayList<>();
-        Player[] data = jsonParser.read("data/players.json");
+        // Initialise Tournament object
+        Tournament tournament = new Tournament();
 
-        // Filter players array down randomly to the specified player count
+        // Generate array of players
+        CSVParser parser = new CSVParser();
+        List<String[]> playerFirstNames = parser.readFromFile("data/firstnames.csv");
+        List<String[]> playerLastNames = parser.readFromFile("data/lastnames.csv");
+
         Random random = new Random();
-        for (int i = 0; i < playerCount - 1; i++) {
-            players.add(data[random.nextInt(data.length)]);
+        for (int i = 0; i < playerCount; i++) {
+            String firstName = playerFirstNames.get(random.nextInt(playerFirstNames.size()))[0];
+            String lastName = playerLastNames.get(random.nextInt(playerLastNames.size()))[0];
+            int elo = 1000 + random.nextInt(500); // Arbitrary range of 1000 to 1500 Elo for any player
+            tournament.addPlayer(firstName, lastName, elo);
         }
 
-        // Order by ELO and create pairings
 
-        // Perform matches
+
+        // Order by ELO and create pairings
+        int byes = tournament.neededByes();
+        System.out.println("Byes needed for this tournament: " + byes);
+        tournament.createPairings(byes);
+
+        /* Perform matches
         Match match = new Match();
         Player winner = match.playMatch(players.get(0), players.get(1));
         System.out.println(winner.getFirstName() + winner.getLastName());
-
+        */
         // Rounds
 
         // Create Tournament class to handle all this stuff
