b5ba78e11b8c337cfb7d1f05719719a8b0f36d05
BFI01 <bfi01messages@gmail.com>
2022-05-04 20:56:17 +0100

    Return loser Player obj from playMatch. Added toString override. Added constructor overload with no args and addPlayer method.

--- src/tournament/Match.java
+++ src/tournament/Match.java
@@ -8,8 +8,8 @@ public class Match extends Game {
     // Using a hashmap rather than storing wins in the player.Player object enforces single responsibility and LoD(?)
     // But makes it so you can't track how many wins each player has after the tournament ends
     private final HashMap<String, Integer> gamesDict = new HashMap<>();
-    private final Player matchPlayer1;
-    private final Player matchPlayer2;
+    private Player matchPlayer1;
+    private Player matchPlayer2;
     private int matchNumber;
     private boolean onGoing = false;
     private Player winner = null;
@@ -24,19 +24,36 @@ public class Match extends Game {
         this.matchPlayer2 = null;
     }
 
+    public Match() {
+        this.matchPlayer1 = null;
+        this.matchPlayer2 = null;
+    }
+
     private void updatePlayer(Player player, int expectedWins, int wins) {
         player.setWins(player.getWins() + 1);
         player.adjustElo(expectedWins, wins);
     }
 
+    public void addPlayer(Player matchPlayer) {
+        if (matchPlayer1 == null) {
+            matchPlayer1 = matchPlayer;
+        } else if (matchPlayer2 == null) {
+            matchPlayer2 = matchPlayer;
+        } else {
+            throw new IndexOutOfBoundsException("This match already has two players!");
+        }
+    }
+
     public Player playMatch() {
+        // Catch bye case
         if (matchPlayer2 == null) {
-            // When a match contains a Bye, the player immediately moves on to the next round
+            // When a match contains a bye, the player immediately moves on to the next round
             System.out.printf("[GAME] %s %s gets a bye and moves on to the next round%n",
                     matchPlayer1.getFirstName(),
                     matchPlayer1.getLastName());
-            return matchPlayer1;
+            return null;
         }
+
 
         gamesDict.put("player1", 0);
         gamesDict.put("player2", 0);
@@ -45,7 +62,7 @@ public class Match extends Game {
         int winsNeeded = (maxGames + 1) / 2;
 
         double eloDifference = matchPlayer1.getElo() - matchPlayer2.getElo();
-        int player1ExpectedWins = (int) Math.floor(getWinChance(eloDifference) * winsNeeded);
+        int player1ExpectedWins = (int) Math.round(getWinChance(eloDifference) * winsNeeded);
 
         do {
             String gameWinner = playGame(matchPlayer1, matchPlayer2);
@@ -60,23 +77,24 @@ public class Match extends Game {
         while (gamesDict.get("player1") < winsNeeded &&
                gamesDict.get("player2") < winsNeeded);
 
-        if (gamesDict.get("player1") > gamesDict.get("player2")) {
-            System.out.printf("[GAME] %s vs %s: %s %s won!%n",
-                    matchPlayer1.getLastName(),
-                    matchPlayer2.getLastName(),
-                    matchPlayer1.getFirstName(),
-                    matchPlayer1.getLastName());
-            updatePlayer(matchPlayer1, player1ExpectedWins, gamesDict.get("player1"));
-            return matchPlayer1;
+        Player loser;
+        if ((gamesDict.get("player1") > gamesDict.get("player2"))) {
+            setWinner(matchPlayer1);
+            loser = matchPlayer2;
         } else {
-            System.out.printf("[GAME] %s vs %s: %s %s won!%n",
-                    matchPlayer1.getLastName(),
-                    matchPlayer2.getLastName(),
-                    matchPlayer2.getFirstName(),
-                    matchPlayer2.getLastName());
-            updatePlayer(matchPlayer2, winsNeeded-player1ExpectedWins, gamesDict.get("player2"));
-            return matchPlayer2;
+            setWinner(matchPlayer2);
+            loser = matchPlayer1;
         }
+
+        System.out.printf("[GAME] %s vs %s: %s %s won!%n",
+                matchPlayer1.getLastName(),
+                matchPlayer2.getLastName(),
+                winner.getFirstName(),
+                winner.getLastName());
+        updatePlayer(matchPlayer1, player1ExpectedWins, gamesDict.get("player1"));
+        updatePlayer(matchPlayer2, winsNeeded-player1ExpectedWins, gamesDict.get("player2"));
+        setOnGoing(false);
+        return loser;
     }
 
     public int getMatchNumber() {
@@ -85,6 +103,22 @@ public class Match extends Game {
 
     public void setMatchNumber(int number) {
         matchNumber = number;
+    }
+
+    public boolean isOnGoing() {
+        return onGoing;
+    }
+
+    public void setOnGoing(boolean onGoing) {
+        this.onGoing = onGoing;
+    }
+
+    public Player getWinner() {
+        return winner;
+    }
+
+    public void setWinner(Player winner) {
+        this.winner = winner;
     }
 
     public String toString() {
@@ -97,17 +131,17 @@ public class Match extends Game {
                     matchPlayer2.getFirstName(),
                     matchPlayer2.getLastName(),
                     matchPlayer2.getElo());
-            if (winner == null) {
+            if (getWinner() == null) {
                 output += String.format(", matchNumber: %d, onGoing: %b, winner: null)",
                         matchNumber,
-                        onGoing);
+                        isOnGoing());
             } else {
                 output += String.format(", matchNumber: %d, onGoing: %b, winner: Player(firstName: %s, lastName: %s, elo: %d))",
                         matchNumber,
-                        onGoing,
-                        winner.getFirstName(),
-                        winner.getLastName(),
-                        winner.getElo());
+                        isOnGoing(),
+                        getWinner().getFirstName(),
+                        getWinner().getLastName(),
+                        getWinner().getElo());
             }
         } else {
             output = String.format("Match(Player(firstName: %s, lastName: %s, elo: %d), Bye",
@@ -116,7 +150,7 @@ public class Match extends Game {
                     matchPlayer1.getElo());
             output += String.format(", matchNumber: %d, onGoing: %b, winner: null)",
                     matchNumber,
-                    onGoing);
+                    isOnGoing());
         }
         return output;
     }
