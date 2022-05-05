f648c0344b558ea514812409c53d61be0e7248d6
BFI01 <bfi01messages@gmail.com>
2022-05-05 02:12:33 +0100

    Minor refactoring

--- src/tournament/Tournament.java
+++ src/tournament/Tournament.java
@@ -117,6 +117,6 @@ class SortByElo implements Comparator<Player> {
 class SortByWins implements Comparator<Player> {
     // Sorts in order from highest to lowest wins
     public int compare(Player player1, Player player2) {
-        return player2.getWins() - player1.getWins();
+        return player2.getMatchWins() - player1.getMatchWins();
     }
 }
